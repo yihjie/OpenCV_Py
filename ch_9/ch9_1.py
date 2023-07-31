7d6d76c54fcbe1c6dc8cc12c1316aa2fd0fff8eb
YIHJIE <yihjie@gmail.com>
2023-07-31 21:38:26 +0800

    1. Studying Chapter 9 2. Prepare the image used in Chapter 9

---
+++ ch_9/ch9_1.py
@@ -0,0 +1,15 @@
+# ret, dst = cv2.threshold(src. thresh, maxval, type)
+
+import cv2
+import numpy as np
+
+thresh = 127
+maxval = 255
+
+src = np.random.randint(0, 256, size=[3, 5], dtype=np.uint8)
+
+ret, dst = cv2.threshold(src, thresh, maxval, cv2.THRESH_BINARY)
+
+print(f"src = \n {src}")
+print(f"thresh = {thresh}, maxval = {maxval}, ret = {ret}")
+print(f"dst = \n {dst}")
