7ee35bc62e3483d843f68eea7b441c2e63846494
YIHJIE <yihjie@gmail.com>
2023-07-31 16:30:17 +0800

    1. cv2.bitwise_and()

---
+++ ch_8/ch8_13.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+src1 = cv2.imread('jk.jpg')
+src2 = np.zeros(src1.shape, dtype=np.uint8)
+src2[30:260, 70:260, :] = 255
+
+dst  = cv2.bitwise_and(src1, src2)
+
+cv2.imshow("src1 - jk", src1)
+cv2.imshow("Mask - src2", src2)
+cv2.imshow("Result - dst", dst)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
