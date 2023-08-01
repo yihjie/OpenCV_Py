39364a361a4d07871adfd8382b72425b0487f12a
YIHJIE <yihjie@gmail.com>
2023-08-01 09:36:55 +0800

    1. Study Chapter 9 2. cv2.threshold(src, threshold, maxvalue, type=cv2.THRESH_BINARY)

---
+++ ch_9/ch9_2.py
@@ -0,0 +1,23 @@
+# cv2.threshold()
+# modified the threshold value
+
+import cv2
+
+maxval = 255
+thresh1 = 127
+thresh2 =  80
+
+src = cv2.imread('jk.jpg', cv2.IMREAD_GRAYSCALE)
+
+ret1, dst1 = cv2.threshold(src, thresh1, maxval, cv2.THRESH_BINARY)
+ret2, dst2 = cv2.threshold(src, thresh2, maxval, cv2.THRESH_BINARY)
+
+cv2.imshow("Original Image", src)
+cv2.imshow(f"DST - {thresh1}", dst1)
+cv2.imshow(f"DST - {thresh2}", dst2)
+
+print(f"thresh1 : {thresh1}\nret1 : {ret1}")
+print(f"thresh2 : {thresh2}\nret2 : {ret2}")
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
