c8b6861e8d7edd9d25ed7a1caea64eb5b0b1c888
YIHJIE <yihjie@gmail.com>
2023-08-01 15:51:25 +0800

    1. cv2.threshold() 2. type = cv2.THRESH_TOZERO

---
+++ ch_9/ch9_12.py
@@ -0,0 +1,21 @@
+# cv2.THRESH_TOZERO
+'''
+    if src > threshold:
+        src = src
+    else
+        src = 0
+'''
+
+import cv2
+import numpy as np
+
+thresh = 127
+maxval = 255
+
+src = np.random.randint(0, 256, size=[3, 5], dtype=np.uint8)
+
+ret, dst = cv2.threshold(src, thresh, maxval, cv2.THRESH_TOZERO)
+
+print(f"src = \n {src}")
+print(f"thresh = {thresh}, maxval = {maxval}, ret = {ret}")
+print(f"dst = \n {dst}")
